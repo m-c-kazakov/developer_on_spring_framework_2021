62ee5122a2899afdf2cfb71ddb8eee86ea452616
Maxim Kazakov <m.c.kazakov.developer@gmail.com>
2022-07-26 12:33:48 +0300

    12_Authentication Настройка WebSecurityConfig

--- 12_Authentication/src/main/kotlin/com/otus/homework/book_catalog_with_mongodb/security/Aasdf.kt
+++
@@ -1,4 +0,0 @@
-package com.otus.homework.book_catalog_with_mongodb.security
-
-class Aasdf {
-}
--- 12_Authentication/src/main/kotlin/com/otus/homework/book_catalog_with_mongodb/security/config/WebSecurityConfig.kt
+++ 12_Authentication/src/main/kotlin/com/otus/homework/book_catalog_with_mongodb/security/config/WebSecurityConfig.kt
@@ -1,17 +1,35 @@
 package com.otus.homework.book_catalog_with_mongodb.security.config
 
 import org.springframework.context.annotation.Bean
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.crypto.password.PasswordEncoder
+import org.springframework.security.web.SecurityFilterChain
 
-class WebSecurityConfig {
+class WebSecurityConfig : WebSecurityConfigurerAdapter() {
 // WebSecurityConfigurerAdapter
+//    @Bean
+//    fun webSecurityCustomizer() : WebSecurityCustomizer {
+//        return WebSecurityCustomizer {
+//            web -> web.i
+//        }
+//    }
+
+
     @Bean
-    fun webSecurityCustomizer() : WebSecurityCustomizer {
-        return WebSecurityCustomizer {
-            web -> web.i
+    fun httpSecurity(httpSecurity: HttpSecurity): SecurityFilterChain {
+
+        httpSecurity.authorizeHttpRequests {
+            it.antMatchers("/login").permitAll()
+            it.anyRequest().authenticated()
         }
+//
+//        httpSecurity.formLogin {
+//            it.loginPage("/login")
+//        }
+
+        return httpSecurity.build()
     }
 
 
