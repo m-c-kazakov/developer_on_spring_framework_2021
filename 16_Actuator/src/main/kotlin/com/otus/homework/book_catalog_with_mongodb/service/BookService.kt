30b9f8e8a27f6d97f84822999103d5b4adea930d
Maxim Kazakov <m.c.kazakov.developer@gmail.com>
2022-07-31 07:51:01 +0300

    16_Actuator оптимизация импортов

--- 16_Actuator/src/main/kotlin/com/otus/homework/book_catalog_with_mongodb/service/BookService.kt
+++ 16_Actuator/src/main/kotlin/com/otus/homework/book_catalog_with_mongodb/service/BookService.kt
@@ -3,7 +3,6 @@ package com.otus.homework.book_catalog_with_mongodb.service
 import com.otus.homework.book_catalog_with_mongodb.dto.BookDtoToCreate
 import com.otus.homework.book_catalog_with_mongodb.dto.BookDtoToUpdate
 import com.otus.homework.book_catalog_with_mongodb.model.Book
-import org.springframework.data.domain.PageRequest
 
 interface BookService {
 
--- 16_Actuator/src/test/kotlin/com/otus/homework/book_catalog_with_mongodb/BookCatalogApplicationTest.kt
+++ 16_Actuator/src/test/kotlin/com/otus/homework/book_catalog_with_mongodb/BookCatalogApplicationTest.kt
@@ -1,5 +1,3 @@
 package com.otus.homework.book_catalog_with_mongodb
 
-import org.junit.jupiter.api.Assertions.*
-
 internal class BookCatalogApplicationTest
